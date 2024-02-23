ee0c88132bd00a50190bb5fe8bf767bc1f200274
Olinka <olga.titzenthaler@gmail.com>
2024-02-23 22:24:39 +0100

    Splneny ukol

--- src/main/java/cz/czechitas/ukol4/Sportka.java
+++ src/main/java/cz/czechitas/ukol4/Sportka.java
@@ -10,7 +10,14 @@ import java.util.List;
 public class Sportka {
     private final List<Integer> osudi = new ArrayList<>();
 
+
     public Sportka() {
+        for (int losovaneCislo = 1; losovaneCislo < 50; losovaneCislo++) {
+
+            osudi.add(losovaneCislo);
+        }
+       // System.out.println(osudi);
+
         //TODO naplnit osudí čísly 1 až 49.
     }
 
@@ -20,9 +27,10 @@ public class Sportka {
      * @see Collections#shuffle(List)
      */
     public void zamichat() {
+        Collections.shuffle(osudi);
+        //System.out.println(osudi);
         //TODO zamíchat osudí
     }
-
     /**
      * Vrátí seznam s 6 vylosovanými čísly.
      *
@@ -30,17 +38,24 @@ public class Sportka {
      * @see List#subList(int, int)
      */
     public List<Integer> vylosovanaCisla() {
-        //TODO Vrátit seznam prvních 6 čísel z osudí.
-        return null;
-    }
 
+        int pocetLosovanychCisel = 6;
+
+        List<Integer> vylosovanaCisla = osudi.subList(0, pocetLosovanychCisel);
+
+        return vylosovanaCisla;
+
+        //TODO Vrátit seznam prvních 6 čísel z osudí.
+
+    }
     /**
      * Vrátí sedmé vylosované číslo (dodatkové číslo)
      *
      * @return Dodatkové číslo.
      */
     public Integer dodatkoveCislo() {
+        int dodatkoveCislo = osudi.get(6);
         //TODO Vrátit sedmé číslo z osudí.
-        return null;
+        return dodatkoveCislo;
     }
 }
